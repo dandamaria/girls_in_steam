8d281a8aa9573eee90c45d04b67cc0ecaf6c3703
DANDARA MARIA DA SILVA FARIAS - SESI <dandafarss@gmail.com>
2024-03-10 00:57:32 -0300

    girlsinsteam

--- home.js
+++ home.js
@@ -27,3 +27,24 @@ function CloseSidebar(){
 }
 
 // js do slide
+
+
+var radio = document.querySelector('.manual-btn')
+var contador = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 4000)
+
+function proximaImg(){
+    contador++
+
+    if (contador > 6){
+        contador = 1
+    }
+
+    document.getElementById('radio'+contador).checked = true
+
+}
